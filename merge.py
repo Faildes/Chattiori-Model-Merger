cf8b39ee5af6016148bf42302614602732d967d1
Chattiori <127287320+Faildes@users.noreply.github.com>
2024-02-08 11:06:45 +0900

    Update merge.py

--- merge.py
+++ merge.py
@@ -245,7 +245,6 @@ parser.add_argument("--keep_ema", action="store_true", help="Keep ema", required
 parser.add_argument("--output", type=str, help="Output file name, without extension", default="merged", required=False)
 parser.add_argument("--functn", action="store_true", help="Add function name to the file", required=False)
 parser.add_argument("--delete_source", action="store_true", help="Delete the source checkpoint file", required=False)
-parser.add_argument("--discard_vae", action="store_true", help="Discard the weights of pre baked vae", required=False)
 parser.add_argument("--device", type=str, help="Device to use, defaults to cpu", default="cpu", required=False)
 
 real_mode = {"WS": "Weighted Sum",
@@ -1353,14 +1352,6 @@ if mode != "NoIn":
         del theta_2
   except NameError:
     pass
-
-if args.discard_vae:
-  theta_X ={}
-  for key in theta_0.keys():
-    if "first_stage_model" in key: continue
-    theta_X[key] = theta_0
-  theta_0 = copy.deepcopy(theta_X)
-  del theta_X
 
 if args.vae is not None:
     print(f"Baking in VAE")
