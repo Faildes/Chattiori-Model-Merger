e895e5e20501561fa7b597202a77847ac61415df
Chattiori / Team-C <127287320+Faildes@users.noreply.github.com>
2024-10-02 13:41:29 +0900

    Delete tq.py

--- tq.py
+++
@@ -1,8 +0,0 @@
-from tqdm.notebook import tqdm as tqdmn
-from tqdm.auto import tqdm as tqdma
-
-def tqdm(*args, **kwargs):
-    try:
-        return tqdmn(*args, **kwargs)
-    except:
-        return tqdma(*args, **kwargs)
