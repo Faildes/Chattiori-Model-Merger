728c3964b0b1c6a0ee62f857166468e305ee861f
Chattiori / Team-C <127287320+Faildes@users.noreply.github.com>
2024-10-02 12:15:38 +0900

    Update merge.py

--- merge.py
+++ merge.py
@@ -13,7 +13,6 @@ import copy
 import argparse
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import scipy.ndimage
 from scipy.ndimage import median_filter as filter
 import re
@@ -547,33 +546,10 @@ def similarity_add_difference(a, b, c, alpha, beta):
     ab_sum = (1 - alpha / 2) * a + (alpha / 2) * b
     return (1 - similarity) * ab_diff + similarity * ab_sum
 
-def resize_tensors(tensor1, tensor2):
-    if len(tensor1.shape) not in [1, 2]:
-        return tensor1, tensor2
-
-    # Pad along the last dimension (width)
-    if tensor1.shape[-1] < tensor2.shape[-1]:
-        padding_size = tensor2.shape[-1] - tensor1.shape[-1]
-        tensor1 = F.pad(tensor1, (0, padding_size, 0, 0))
-    elif tensor2.shape[-1] < tensor1.shape[-1]:
-        padding_size = tensor1.shape[-1] - tensor2.shape[-1]
-        tensor2 = F.pad(tensor2, (0, padding_size, 0, 0))
-
-    # Pad along the first dimension (height)
-    if tensor1.shape[0] < tensor2.shape[0]:
-        padding_size = tensor2.shape[0] - tensor1.shape[0]
-        tensor1 = F.pad(tensor1, (0, 0, 0, padding_size))
-    elif tensor2.shape[0] < tensor1.shape[0]:
-        padding_size = tensor1.shape[0] - tensor2.shape[0]
-        tensor2 = F.pad(tensor2, (0, 0, 0, padding_size))
-
-    return tensor1, tensor2
-
 def dare_merge(theta0, theta1, alpha):
-    t0n, t1n = resize_tensors(theta0, theta1)
-    m = torch.bernoulli(torch.full_like(input=t0n.float(), fill_value=0.5), generator=rand_generator)
+    m = torch.bernoulli(torch.full_like(input=theta0.float(), fill_value=0.5), generator=rand_generator)
     alpha = torch.mul(m, alpha / 0.5)
-    return torch.lerp(t0n.float(), t1n.float(), alpha).to(theta0.dtype)
+    return torch.lerp(theta0.float(), theta1.float(), alpha).to(theta0.dtype)
 
 def prune_model(theta, name, isxl=False):
     sd_pruned = dict()
@@ -1296,4 +1272,3 @@ else:
 del theta_0
 file_size = round(os.path.getsize(output_path) / 1073741824,2)
 print(f"Done! ({file_size}G)")
-
