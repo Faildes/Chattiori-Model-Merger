f6ef78c29299ab8df25c5c98fd930e7eec9f198a
Chattiori / Team-C <127287320+Faildes@users.noreply.github.com>
2024-10-02 13:14:08 +0900

    Update tq.py

--- tq.py
+++ tq.py
@@ -1,9 +1,8 @@
 from tqdm.notebook import tqdm as tqdmn
 from tqdm.auto import tqdm as tqdma
 
-class tqdm:
-    def __init__(self, *args, **kwargs):
-        try:
-            return tqdmn(*args, **kwargs)
-        except:
-            return tqdma(*args, **kwargs)
+def tqdm(self, *args, **kwargs):
+    try:
+        return tqdmn(*args, **kwargs)
+    except:
+        return tqdma(*args, **kwargs)
